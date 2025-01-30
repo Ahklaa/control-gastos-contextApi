78d472517ae3e5dde0cd4fe6e9869fdfc1d512d6
Ahkla <ahkla185@hotmail.com>
2025-01-30 16:52:44 -0500

    se avanzo en el componente y se creo un helper de formatCurrency, se creo el AmountDisplay para mostrar los valores y se inserto un grafico de ejemplo

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,5 +1,28 @@
+import AmountDisplay from "./AmountDisplay";
+
 export default function BudgetTracker() {
   return (
-    <div>BudgetTracker</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="flex justify-center">
+         <img alt ="Gráfico presupuesto" src="/grafico.jpg"/>
+        </div>
+        <div className="flex flex-col items-center gap-8">
+            <button className="bg-pink-600 font-bold text-white p-2 w-full uppercase rounded-lg">
+                Resetear App
+            </button>
+            <AmountDisplay 
+                label = "Presupuesto"
+                amount = {300}
+            />
+            <AmountDisplay 
+                label = "Disponible"
+                amount = {200}
+            />
+            <AmountDisplay 
+                label = "Gastado"
+                amount = {100}
+            />
+        </div>
+    </div>
   )
 }
