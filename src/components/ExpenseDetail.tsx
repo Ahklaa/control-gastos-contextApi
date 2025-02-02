e94d70696a6f781d56430f76b976aecf15ef42a7
Ahkla <ahkla185@hotmail.com>
2025-02-02 15:48:25 -0500

    se agregaron dos compontentes el react-swipeable-list y el prop-types para evitar errores de estilos,  se encerro todo el codigo de expense details en un SwipeableList y en un SwipeableListItem donde se le pasan los parametros de maxWipe(parara la catnidad de pixeles) el leadingActios y el trailingActions que es para poder ejecutar en caso de moverlo a la izquierda y derecha, ademas se creo una funcion para cada uno de estos, la cual esta rodeada por el nombre de Trailing/leading actions junto con Swipe action que recibe un onclick con la funcion que espera a futuro en este caso agregar y actualizar, SE IMPORTaron estilos para estos los cuales traje de git

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -3,23 +3,55 @@ import { formatDate } from "../helpers"
 import type { Expense } from "../types"
 import AmountDisplay from "./AmountDisplay"
 import { categories } from "../data/category"
+import {
+    SwipeableList,
+    SwipeableListItem,
+    TrailingActions,
+    LeadingActions,
+    SwipeAction
+} from "react-swipeable-list"
+import 'react-swipeable-list/dist/styles.css';
+
 type ExpenseDetailProps = {
     expense : Expense
     
 }
 export default function ExpenseDetail({expense}: ExpenseDetailProps) {
     const categoryInfo = useMemo(()=> categories.filter(cat => cat.id === expense.category),[expense])[0]
+
+    const leadingActionExpense = () => (
+        <LeadingActions>
+            <SwipeAction onClick={()=> {}}>
+                Actualizar
+            </SwipeAction>
+        </LeadingActions>
+    )
+    const trailingActionExpense = () => (
+        <TrailingActions>
+            <SwipeAction onClick={()=> {}} destructive={true}>
+                Eliminar
+            </SwipeAction>
+        </TrailingActions>
+    )
   return (
-   <div className="bg-white shadow-lg border-b border-gray-200 p-10 w-full flex items-center gap-5">
-        <div>
-            <img src={`/icono_${categoryInfo.icon}.svg`} alt="icono gasto" className="w-20"/>
-        </div>
-        <div className="flex-1 space-y-2">
-            <p className="text-slate-500 uppercase font-bold">{categoryInfo.name}</p>
-            <p className="">{expense.expenseName}</p>
-            <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
-        </div>
-        <AmountDisplay amount={expense.amount}/>
-   </div>
+    <SwipeableList>
+        <SwipeableListItem 
+        maxSwipe={30}
+        leadingActions={leadingActionExpense()}
+        trailingActions={trailingActionExpense()}
+        >
+            <div className="bg-white shadow-lg border-b border-gray-200 p-10 w-full flex items-center gap-5">
+                    <div>
+                        <img src={`/icono_${categoryInfo.icon}.svg`} alt="icono gasto" className="w-20"/>
+                    </div>
+                    <div className="flex-1 space-y-2">
+                        <p className="text-slate-500 uppercase font-bold">{categoryInfo.name}</p>
+                        <p className="">{expense.expenseName}</p>
+                        <p className="text-slate-600 text-sm">{formatDate(expense.date!.toString())}</p>
+                    </div>
+                    <AmountDisplay amount={expense.amount}/>
+            </div>
+        </SwipeableListItem>
+   </SwipeableList>
   )
 }
